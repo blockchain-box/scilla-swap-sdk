f592e3fae13c8f8a187e218ded987a750dce8491
anas <anastal2001@yahoo.com>
2022-01-02 17:48:14 +0100

    WIP

--- src/dto/CalculateSwapResultDTO.js
+++
@@ -1,9 +0,0 @@
-module.exports = class CalculateSwapResultDTO {
-    constructor({forAddress, fromToken, toToken, fromAmount, isTransfer}) {
-        this.forAddress = forAddress;
-        this.fromToken = fromToken;
-        this.toToken = toToken;
-        this.fromAmount = fromAmount;
-        this.isTransfer = isTransfer;
-    }
-}
--- src/dto/index.js
+++ src/dto/index.js
@@ -2,6 +2,4 @@ module.exports = {
     GetTokenOfAccountInSwapDTO: require("./GetTokenOfAccountInSwapDTO"),
     GetPoolsOfAccountDTO: require("./GetPoolsOfAccountDTO"),
     IsTokenUnLockedDTO: require("./IsTokenUnLockedDTO"),
-    CalculateSwapResultDTO: require("./CalculateSwapResultDTO"),
-    CalculateSwapParamsDTO: require("./CalculateSwapParamsDTO"),
 }
