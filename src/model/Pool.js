9075994b351fcc58ceee6377096ec906e621bf44
anas <anastal2001@yahoo.com>
2022-01-03 15:59:54 +0100

    WIP

--- src/model/Pool.js
+++ src/model/Pool.js
@@ -1,20 +1,9 @@
 const Token = require("./Token");
 module.exports = class Pool {
-    constructor({carbAmount, tokenAmount, token = new Token({})}) {
-        this._carbAmount = carbAmount;
-        this._tokenAmount = tokenAmount;
-        this._token = token;
-    }
-
-    get carbAmount() {
-        return this._carbAmount;
-    }
-
-    get tokenAmount() {
-        return this._tokenAmount;
-    }
-
-    get token() {
-        return this._token;
+    constructor({totalContribution, carbAmount, tokenAmount, token = new Token({})}) {
+        this.totalContribution = totalContribution;
+        this.carbAmount = carbAmount;
+        this.tokenAmount = tokenAmount;
+        this.token = token;
     }
 }
